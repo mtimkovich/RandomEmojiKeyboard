e9ca24b7148b6f4957880134d94040beef75646e
Max Timkovich <mtimkovich@gmail.com>
2017-04-21 01:30:35 -0700

    added asynctask for deleting multiple

--- app/src/main/java/com/maxtimkovich/randomemojikeyboard/RandomEmojiKeyboard.java
+++ app/src/main/java/com/maxtimkovich/randomemojikeyboard/RandomEmojiKeyboard.java
@@ -1,17 +1,58 @@
 package com.maxtimkovich.randomemojikeyboard;
 
 import android.inputmethodservice.InputMethodService;
+import android.os.AsyncTask;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
+import android.widget.ImageButton;
 
 import java.util.Random;
 
 public class RandomEmojiKeyboard extends InputMethodService {
+    boolean pressed = false;
+
+    class Delete extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            InputConnection ic = getCurrentInputConnection();
+            while (pressed) {
+                ic.deleteSurroundingText(2, 0);
+
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            return null;
+        }
+    }
+
     @Override
     public View onCreateInputView() {
         View v = getLayoutInflater().inflate(R.layout.keyboard, null);
-        v.findViewById(R.id.backspace);
+        ImageButton backspace = (ImageButton) v.findViewById(R.id.backspace);
+        backspace.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        if (!pressed) {
+                            pressed = true;
+                            new Delete().execute();
+                        }
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        pressed = false;
+                        break;
+                }
+                return true;
+            }
+        });
 
         return v;
     }
@@ -20,9 +61,6 @@ public class RandomEmojiKeyboard extends InputMethodService {
         InputConnection ic = getCurrentInputConnection();
 
         switch (view.getId()) {
-            case R.id.backspace:
-                ic.deleteSurroundingText(2, 0);
-                break;
             case R.id.enter:
                 ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
                 break;
